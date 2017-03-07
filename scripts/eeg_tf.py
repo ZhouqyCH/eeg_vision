25ba0e7bc7679f1be1075fbe870355865006ee06
cgcarvalhaes <c.g.carvalhaes@gmail.com>
2017-03-07 06:04:16 -0800

    Restores eeg_tf.py to its first version

--- scripts/eeg_tf.py
+++ scripts/eeg_tf.py
@@ -33,26 +33,28 @@ if __name__ == '__main__':
     file_name = os.path.join(settings.PATH_TO_MAT_FILES, args.subject.upper() + ".mat")
     ds = build_data_sets(file_name, avg_group_size=5, derivation='electric_field', random_state=42, test_proportion=0.2)
 
-    # The input x will consist of a 2d tensor of floating point numbers
+    # The input x will consist of a tensor of floating point numbers
     x = tf.placeholder(tf.float32, shape=[None, ds.train.n_channels, ds.train.trial_size, ds.train.n_comps])
+    # The target output classes y_ will consist of a 2d tensor, where each row is a one-hot 6-dimensional vector
+    # indicating which digit class (zero through 5) the corresponding trial belongs to
     y_ = tf.placeholder(tf.float32, shape=[None, ds.train.n_class])
 
     # First Convolutional Layer.
-    # It will compute 20 features for each 5x5 patch. Its weight tensor will have a shape of
-    # [5, 5, 3, 20]. The first two dimensions are the patch size, the next is the number of input channels, and the
-    # last is the number of output channels
-    W_conv1 = weight_variable([4, 4, 3, 10])
+    # It will compute 32 features for each 5x5 patch. Its weight tensor will have a shape of
+    # [5, 5, 3, 32]. The first two dimensions are the patch size, the next is the number of electric field components,
+    # and the last is the number of output components
+    W_conv1 = weight_variable([5, 5, 3, 32])
     # a bias vector with a component for each output channel
-    b_conv1 = bias_variable([10])
+    b_conv1 = bias_variable([32])
     # Apply the layer by convolving x with the weight tensor, add the bias, apply the ReLU function, and finally max
     # pool.
     h_conv1 = tf.nn.relu(conv2d(x, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
     # Second Convolutional Layer.
-    # Stacks a second layer that provides 16 features for each 5x5 patch
-    W_conv2 = weight_variable([4, 4, 10, 16])
-    b_conv2 = bias_variable([16])
+    # Stacks a second layer that provides 64 features for each 5x5 patch
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
@@ -60,10 +62,10 @@ if __name__ == '__main__':
     # Densely Connected Layer. The image size has been reduced to 7x7. A fully-connected layer with 1024 neurons is
     # added to allow processing on the entire image. The tensor from the pooling layer is reshaped into a batch of
     # vectors, multiplied by a weight matrix, added to a bias, and applied to a ReLU
-    W_fc1 = weight_variable([31 * 8 * 16, 10])
-    b_fc1 = bias_variable([10])
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 31 * 8 * 16])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # Dropout. To reduce overfitting, we will apply dropout before the readout layer. We create a placeholder for the
@@ -75,7 +77,7 @@ if __name__ == '__main__':
 
     # Readout Layer
     # Finally, we add a layer, just like for the one layer softmax regression above.
-    W_fc2 = weight_variable([10, ds.train.n_class])
+    W_fc2 = weight_variable([1024, ds.train.n_class])
     b_fc2 = bias_variable([ds.train.n_class])
 
     # implements the convolutional model
@@ -101,7 +103,7 @@ if __name__ == '__main__':
         max_iter = 20000
         print "Training the network with a maximum of %s iterations" % max_iter
         for i in range(max_iter):
-            batch = ds.train.next_batch(20)
+            batch = ds.train.next_batch(50)
             if i % 100 == 0:
                 train_accuracy = accuracy.eval(feed_dict={
                     x: batch[0], y_: batch[1], keep_prob: 1.0})
